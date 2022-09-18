73df12b7d47e2c5ee107a4e44b334841a302b3e8
Suchan An <dobestan@gmail.com>
2022-09-18 13:45:13 +0900

    Update Soulbound: SBT token must mint once per soul

--- contracts/Soulbound.sol
+++ contracts/Soulbound.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract Soulbound is ERC5633, Ownable {
-    uint public constant MALICIOUS = 0;
+    uint public constant REGISTERED = 0;
 
     // BDSM Tokens(Score): 0-100(Default: 50)
     uint public constant BD_B = 1;
@@ -17,22 +17,34 @@ contract Soulbound is ERC5633, Ownable {
     uint public constant SM_S = 5;
     uint public constant SM_M = 6;
 
+    uint public constant MALICIOUS = 7;
+
     constructor() ERC5633("") {
-        _setSoulbound(MALICIOUS, true);
+        _setSoulbound(REGISTERED, true);
         _setSoulbound(BD_B, true);
         _setSoulbound(BD_D, true);
         _setSoulbound(DS_D, true);
         _setSoulbound(DS_S, true);
         _setSoulbound(SM_S, true);
         _setSoulbound(SM_M, true);
+        _setSoulbound(MALICIOUS, true);
     }
 
-    function register(address soul, uint[6] calldata bdsm_score) external onlyOwner {
+    function register(address soul, uint[6] calldata bdsm_score) external onlyOwner soulNotRegistered(soul) {
+        _mint(soul, REGISTERED, 1, "");
         _mint(soul, BD_B, bdsm_score[0], "");
         _mint(soul, BD_D, bdsm_score[1], "");
         _mint(soul, DS_D, bdsm_score[2], "");
         _mint(soul, DS_S, bdsm_score[3], "");
         _mint(soul, SM_S, bdsm_score[4], "");
         _mint(soul, SM_M, bdsm_score[5], "");
+    }
+
+    modifier soulNotRegistered(address soul) {
+        require(
+            balanceOf(soul, REGISTERED) == 0,
+            "Soulbound: Soul is already registered"
+        );
+        _;
     }
 }
