1d3b4a569541aac58a752e4cec50cc974938af6f
Suchan An <dobestan@gmail.com>
2022-09-18 16:14:13 +0900

    Initialize hardhat-deploy: https://github.com/wighawag/hardhat-deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-deploy');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
+    namedAccounts: {
+        deployer: 0,
+    }
 };
