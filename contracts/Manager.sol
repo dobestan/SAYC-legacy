8c49b30a8ab688a2405b4d5cba2e283b125f96d2
Suchan An <dobestan@gmail.com>
2022-09-18 17:37:04 +0900

    Update Manager: add createMatch, and createMatches for batch creation

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -3,10 +3,19 @@ pragma solidity >=0.8.0;
 
 
 import "./Soulbound.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract Manager {
+contract Manager is Ownable {
     address public soulbound;
+    Match[] public matches; 
+
+    struct Match {
+        address account0;
+        address account1;
+    }
+
+    event Matched(address indexed accountA, address indexed accountB);
 
     constructor() {
         soulbound = address(new Soulbound());
@@ -14,5 +23,31 @@ contract Manager {
 
     function register(uint[6] calldata bdsm_score) external {
         Soulbound(soulbound).register(msg.sender, bdsm_score);
+    }
+
+    function _asOrderedAddresses(address accountA, address accountB) public pure returns (address account0, address account1) {
+        // account0/account1 is Ordered
+        // accountA/accountB is Unordered
+        return accountA < accountB ? (accountA, accountB) : (accountB, accountA);
+    }
+
+    function createMatch(address accountA, address accountB) public onlyOwner {
+        (address address0, address address1) = _asOrderedAddresses(accountA, accountB);
+        Match memory createdMatch = Match(address0, address1);
+        matches.push(createdMatch);
+
+        emit Matched(address0, address1);
+        emit Matched(address1, address0);
+    }
+
+    function createMatches(address[] calldata accountAs, address[] calldata accountBs) public onlyOwner {
+        require(
+            accountAs.length == accountBs.length,
+            "Manager: matching addresses length is mismatched"
+        );
+
+        for (uint i; i < accountAs.length; i++) {
+            createMatch(accountAs[i], accountBs[i]);
+        }
     }
 }
---
+++ scripts/create_batch_matches.js
@@ -0,0 +1,30 @@
+const hre = require("hardhat");
+
+
+async function main() {
+    const signers = await ethers.getSigners();
+    const deployed = await hre.deployments.all();
+
+    const Manager = await hre.ethers.getContractFactory("Manager");
+    const manager = await Manager.attach(deployed.Manager.address);
+
+    const Soulbound = await hre.ethers.getContractFactory("Soulbound");
+    const soulbound = await Soulbound.attach(await manager.soulbound());
+
+    console.log("[Event Listening] Manager:Matched");
+    manager.on("Matched", (accountA, accountB) => {
+        console.log(`[Event emitted] Manager:Matched(${accountA}, ${accountB})`);
+    });
+
+    manager.createMatch(signers[0].address, signers[1].address);
+    manager.createMatches(
+        [signers[0].address, signers[1].address],
+        [signers[2].address, signers[3].address],
+    );
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
